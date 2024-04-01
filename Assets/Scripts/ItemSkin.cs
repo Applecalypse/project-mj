f2b36ffda91735f6f93c186a82768c2b728b407b
thanawinboon <pokepat27@gmail.com>
2024-04-01 07:02:48 +0700

    added a script for items with multiple skins / aesthetical variation

--- Assets/Scripts/ItemSkin.cs
+++ Assets/Scripts/ItemSkin.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class ItemSkin : MonoBehaviour
 {
-    
+    [Header("Item Skin Settings")]
+    [SerializeField] private MeshFilter defaultMeshFilter;
+    [SerializeField] private List<Mesh> allSkins = new List<Mesh>();
+
+    [Header("Item Skin Options")]
+    [SerializeField] private bool randomizeSkin = false;
+
     void Start()
     {
-        
+        if (defaultMeshFilter == null)
+        {
+            Debug.LogError("ItemSkin: Default Mesh Filter is not assigned");
+            return;
+        }
+
+        if (allSkins.Count == 0)
+        {
+            Debug.LogError("ItemSkin: List of skins is empty");
+            return;
+        }
+
+        if (randomizeSkin)
+        {
+            int randomSkinIndex = Random.Range(0, allSkins.Count);
+            defaultMeshFilter.mesh = allSkins[randomSkinIndex];
+        }
     }
 }
