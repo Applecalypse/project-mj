91c7f40fb0f1a153f2a8f967a2258b5c4c9a22c3
Suphawit <suphawit.won@gmail.com>
2024-04-05 01:01:40 +0700

    hot fix onPlayerDespawn

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -134,7 +134,6 @@ public class PlayerController : NetworkBehaviour
     {
         GameManager gameManager = FindObjectOfType<GameManager>();
         gameManager.EmptyPosition(sittingPos.position, sittingPos.rotation);
-        GameManager.Instance.uidToTeam.Remove(OwnerClientId);
         base.OnNetworkDespawn();
     }
 
